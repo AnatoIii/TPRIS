76992c56830476c4c6ca4b3ac2eab4b4ce8041fb
Anatolii Ivanov <anatolii-ivanov@binary-studio.com>
2022-12-29 23:36:42 +0200

    Some updates

--- frontend/src/app/_services/auth.service.ts
+++ frontend/src/app/_services/auth.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-const AUTH_API = 'http://localhost:8080/api/auth/';
+import { API_URL } from '../env';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -15,14 +14,14 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
+    return this.http.post(API_URL + 'users/login', {
       username,
       password
     }, httpOptions);
   }
 
   register(username: string, email: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signup', {
+    return this.http.post(API_URL + 'users/register', {
       username,
       email,
       password
